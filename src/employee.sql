8cc2d8cb5c7eb2bc2c66950548d07d620bef35d3
liujie <1360226126@qq.com>
2018-10-22 19:36:39 +0800

    利用servlet和jquery+c3p0+hightcharts做一个图标的小例子

---
+++ src/employee.sql
@@ -0,0 +1,4 @@
+INSERT INTO `employee`(`id`, `name`, `gender`, `salary`) VALUES (1, '孙悟空', '男', 8000.00);
+INSERT INTO `employee`(`id`, `name`, `gender`, `salary`) VALUES (2, '周丽丽', '女', 5000.00);
+INSERT INTO `employee`(`id`, `name`, `gender`, `salary`) VALUES (3, '范冰冰', '女', 6000.00);
+INSERT INTO `employee`(`id`, `name`, `gender`, `salary`) VALUES (4, '李冰冰', '女', 899899.00);
